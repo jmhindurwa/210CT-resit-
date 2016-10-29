230063f1303764e8f91ee972babd48e66ae94e5e
jmhindurwa <jmhindurwa@ymail.com>
2016-10-29 10:37:16 +0100

    Update and rename Task 1  Part 1 code.py to Task 1.py

--- Task 1  Part 1 code.py
+++
@@ -1,8 +0,0 @@
-Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import random
->>> a=[1,2,3,4,5,6,7,8,9,]
->>> random.shuffle(a,random.random)
->>> a
-[4, 7, 1, 9, 6, 2, 5, 8, 3]
->>> 
---
+++ Task 1.py
@@ -0,0 +1,6 @@
+>>> import random
+>>> a=[1,2,3,4,5,6,7,8,9,]
+>>> random.shuffle(a,random.random)
+>>> a
+[4, 7, 1, 9, 6, 2, 5, 8, 3]
+>>> 
